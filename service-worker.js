6119b8da371c2cc36956bd5aebcbf7cbef8aa17c
AkshayAghera <akshayaghera85@gmail.com>
2025-12-23 19:51:43 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+// IMPORTANT:
+// Increment cache version on EVERY UI or logic change
+const CACHE_NAME = 'ems-cache-v4';
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-72x72.png',
+  '/icon-96x96.png',
+  '/icon-128x128.png',
+  '/icon-144x144.png',
+  '/icon-152x152.png',
+  '/icon-192x192.png',
+  '/icon-384x384.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
